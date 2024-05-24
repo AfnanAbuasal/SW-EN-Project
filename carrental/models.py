7958b692f9f4e7c337d952dd111b9b35efd7b2e3
Lily010304 <layla.alsaabna@icloud.com>
2024-05-24 06:28:50 +0300

    Added style sheet and modified the web page

--- carrental/models.py
+++ carrental/models.py
@@ -9,13 +9,16 @@ class Lessor(models.Model):
     name = models.CharField(max_length=64)
     phone = models.IntegerField(null=False, blank=False, unique=True)
     email = models.EmailField(max_length = 254)
+    
+    def __str__(self):
+        return f"{self.name}"
  
 
 class Vehicle(models.Model):
     vehicle_id = models.IntegerField(unique= True, auto_created= True, primary_key=True, max_length=64)
     lessor_id = models.ForeignKey(Lessor, on_delete= models.CASCADE)
     genre = models.CharField(max_length=64)
-    image = models.ImageField(blank = True, null = True, upload_to="locations/%Y/%m/%D")
+    image = models.URLField(blank = True, null = True, max_length=255)
     no_of_seats = models.IntegerField(max_length=8)
     model = models.CharField(max_length=64)
     color = models.CharField(max_length=64)
@@ -25,6 +28,9 @@ class Vehicle(models.Model):
     fuel_policy = models.CharField(max_length=50)
     price_per_day = models.IntegerField(max_length=64)
     
+    def __str__(self):
+        return f"{self.model}"
+    
    
 class Customer(models.Model):
     customer_id = models.IntegerField(unique= True, auto_created= True, primary_key=True, max_length=64)
@@ -32,12 +38,18 @@ class Customer(models.Model):
     phone = models.IntegerField(null=False, blank=False, unique=True)
     email = models.EmailField(max_length = 254)
     
+    def __str__(self):
+        return f"{self.name}"
+    
     
    
 class Renting(models.Model):
     rent_id= models.IntegerField(unique= True, auto_created= True, primary_key=True, max_length=64)
     vehicle_id = models.OneToOneField(Vehicle, on_delete= models.CASCADE)
     customer_id = models.OneToOneField(Customer, on_delete= models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.rent_id}"
     
 
 
--- carrental/views.py
+++ carrental/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from .models import Lessor, Vehicle, Renting, Customer
-from django.http import  HttpResponse, HttpResponseRedirect
+
 
 
 # Create your views here.
 
 def index(request):
     return render(request, "carrental/index.html",{
-        "vehicle" : Vehicle.objects.all()})
+        "vehicles" : Vehicle.objects.all()})
